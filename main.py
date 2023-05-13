c878726e957f9cfba0c2cd572649b7e6a2c92719
brandon-k23 <brandon.kong@stonybrook.edu>
2023-05-13 01:50:30 +0000

    drop nan

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import subprocess
 subprocess.run(['pip', 'install', '-r', 'requirements.txt'])
 from fastapi import FastAPI
-import pandas as pd 
+import pandas as pd
+import numpy as np
 
-df = pd.read_csv('./data/smallDiagnosis2019.csv', low_memory=False)
-
+df = pd.read_csv('./data/smallDiagnosis2019.csv')
+df.replace('', np.nan, inplace=True) # replace empty with nan (not a number)
+df.dropna(inplace=True) # drop all nans
 app = FastAPI()
 
 @app.get("/")
